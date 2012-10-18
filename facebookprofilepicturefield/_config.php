436bc05cb2390f0d414ed535a34c70bf2f710a9a
Will Morgan <avengex@gmail.com>
2012-10-18 12:31:23 +0100

    Oops, moving outside of folder

--- facebookprofilepicturefield/_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// hat tip: @dhensby
-define('MOD_FPPF_PATH',rtrim(dirname(__FILE__), DIRECTORY_SEPARATOR));
-$folders = explode(DIRECTORY_SEPARATOR,MOD_FPPF_PATH);
-define('MOD_FPPF_DIR',rtrim(array_pop($folders),DIRECTORY_SEPARATOR));
-unset($folders);
-
